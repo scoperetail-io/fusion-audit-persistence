2c79a36788da0f7910789193a3d5f17134910b81
Javier Casanova Scoperetail <javier.casanova@scoperetail.com>
2021-05-06 18:18:56 -0500

    Setting duplicate insert query and dedupe entity

--- src/main/java/com/scoperetail/fusion/adapter/out/persistence/jpa/repository/DedupeKeyRepository.java
+++ src/main/java/com/scoperetail/fusion/adapter/out/persistence/jpa/repository/DedupeKeyRepository.java
@@ -1,9 +1,9 @@
 /* ScopeRetail (C)2021 */
 package com.scoperetail.fusion.adapter.out.persistence.jpa.repository;
 
-import com.scoperetail.fusion.adapter.out.persistence.jpa.entity.MessageLogEntity;
+import com.scoperetail.fusion.adapter.out.persistence.jpa.entity.DedupeKeyEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface DedupeKeyRepository extends JpaRepository<MessageLogEntity, String> {}
+public interface DedupeKeyRepository extends JpaRepository<DedupeKeyEntity, String> {}

a554fdfacd80ba7e3983366e015291c68c5d29d2
Javier Casanova Scoperetail <javier.casanova@scoperetail.com>
2021-05-11 18:48:44 -0500

    Insert if not exist instead of search for logKey

--- src/main/java/com/scoperetail/fusion/adapter/out/persistence/jpa/repository/DedupeKeyRepository.java
+++ src/main/java/com/scoperetail/fusion/adapter/out/persistence/jpa/repository/DedupeKeyRepository.java
@@ -3,7 +3,17 @@ package com.scoperetail.fusion.adapter.out.persistence.jpa.repository;
 
 import com.scoperetail.fusion.adapter.out.persistence.jpa.entity.DedupeKeyEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import javax.transaction.Transactional;
+
 @Repository
-public interface DedupeKeyRepository extends JpaRepository<DedupeKeyEntity, String> {}
+public interface DedupeKeyRepository extends JpaRepository<DedupeKeyEntity, String> {
+  @Transactional
+  @Modifying
+  @Query(name = "dedupeKey.jpa.insert", nativeQuery = true)
+  Integer insertIfNotExist(@Param("logKey") String logKey);
+}

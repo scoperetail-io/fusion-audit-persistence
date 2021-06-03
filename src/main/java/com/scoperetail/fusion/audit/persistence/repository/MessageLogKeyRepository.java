557ae291748a0baaaf4f9702463790105785d8af
ravi-zadpide <ravi.zadpide@scoperetail.com>
2021-06-03 12:23:25 +0530

    Framework changes.

--- src/main/java/com/scoperetail/fusion/audit/persistence/repository/MessageLogKeyRepository.java
+++ src/main/java/com/scoperetail/fusion/audit/persistence/repository/MessageLogKeyRepository.java
@@ -1,9 +1,9 @@
 /* ScopeRetail (C)2021 */
-package com.scoperetail.fusion.adapter.out.persistence.jpa.repository;
+package com.scoperetail.fusion.audit.persistence.repository;
 
-import com.scoperetail.fusion.adapter.out.persistence.jpa.entity.MessageLogKeyEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import com.scoperetail.fusion.audit.persistence.entity.MessageLogKeyEntity;
 
 @Repository
 public interface MessageLogKeyRepository extends JpaRepository<MessageLogKeyEntity, String> {}

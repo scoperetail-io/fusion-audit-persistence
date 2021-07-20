3b51a248feb5cbc11e9f76c25a0497d2cb0c21f9
Javier Rivera Casanova <javier.casanova@scoperetail.com>
2021-07-20 18:29:41 -0500

    Purge queries for auditor tables

--- src/main/java/com/scoperetail/fusion/audit/persistence/repository/MessageLogKeyRepository.java
+++ src/main/java/com/scoperetail/fusion/audit/persistence/repository/MessageLogKeyRepository.java
@@ -1,9 +1,39 @@
-/* ScopeRetail (C)2021 */
 package com.scoperetail.fusion.audit.persistence.repository;
 
+/*-
+ * *****
+ * fusion-audit-persistence
+ * -----
+ * Copyright (C) 2018 - 2021 Scope Retail Systems Inc.
+ * -----
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * =====
+ */
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import com.scoperetail.fusion.audit.persistence.entity.MessageLogKeyEntity;
 
+import javax.transaction.Transactional;
+import java.util.List;
+
 @Repository
-public interface MessageLogKeyRepository extends JpaRepository<MessageLogKeyEntity, String> {}
+public interface MessageLogKeyRepository extends JpaRepository<MessageLogKeyEntity, String> {
+  @Query(name = "delete.message.log.key")
+  @Modifying
+  @Transactional
+  Integer deleteMessageLogKey(@Param("logKeyList") List<String> logKeyList);
+}
--- src/main/java/com/scoperetail/fusion/audit/persistence/repository/MessageLogRepository.java
+++ src/main/java/com/scoperetail/fusion/audit/persistence/repository/MessageLogRepository.java
@@ -1,9 +1,31 @@
-/* ScopeRetail (C)2021 */
 package com.scoperetail.fusion.audit.persistence.repository;
 
+/*-
+ * *****
+ * fusion-audit-persistence
+ * -----
+ * Copyright (C) 2018 - 2021 Scope Retail Systems Inc.
+ * -----
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * =====
+ */
+
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 import javax.transaction.Transactional;
+
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
@@ -25,4 +47,12 @@ public interface MessageLogRepository extends JpaRepository<MessageLogEntity, In
 
   @Query(name = "messageLog.jpa.findByLogKey")
   Optional<MessageLogEntity> findByLogKey(@Param("logKey") String logKey);
+
+  @Query(name = "message.log.keys.to.erase")
+  List<String> findLogKeysToErase(LocalDateTime pivoteDate, Pageable pageable);
+
+  @Query(name = "delete.message.log")
+  @Modifying
+  @Transactional
+  Integer deleteMessageLog(@Param("logKeyList") List<String> logKeyList);
 }
